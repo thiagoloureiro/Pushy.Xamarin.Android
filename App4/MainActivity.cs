fcdbe31845c6e55b955364723034e7315a4b8b03
PLTHLOU <thiago.loureiro-de-azevedo@pl.abb.com>
2021-07-21 09:37:32 +0200

    Initial Commit

---
+++ App4/MainActivity.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using ME.Pushy.Sdk;
+using System.Threading.Tasks;
+using Java.Net;
+
+namespace App4
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Pushy.Listen(this);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            RegisterForPushNotifications();
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        private async Task RegisterForPushNotifications()
+        {
+            // Execute Pushy.Register() in a background thread
+            await Task.Run(() =>
+            {
+                try
+                {
+                    // Assign a unique token to this device
+                    string deviceToken = Pushy.Register(this);
+
+                    // Log it for debugging purposes
+                    //Log.Debug("MyApp", "Pushy device token: " + deviceToken);
+
+                    // Send the token to your backend server via an HTTP GET request
+                    //new URL("https://{YOUR_API_HOSTNAME}/register/device?token=" + deviceToken).OpenConnection();
+                }
+                catch (Exception exc)
+                {
+                    // Log error to console
+                    // Log.Error("MyApp", exc.Message, exc);
+                    return;
+                }
+
+                // Succeeded, optionally do something to alert the user
+            });
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ App4/PushReceiver.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Media;
+using ME.Pushy.Sdk;
+
+namespace App4
+{
+    [BroadcastReceiver(Enabled = true, Exported = false)]
+    [IntentFilter(new[] { "pushy.me" })]
+    public class PushReceiver : BroadcastReceiver
+    {
+        [System.Obsolete]
+        public override void OnReceive(Context context, Intent intent)
+        {
+            string notificationTitle = "MyApp";
+            string notificationText = "Test notification";
+
+            // Attempt to extract the "message" property from the payload: {"message":"Hello World!"}
+            if (intent.GetStringExtra("message") != null)
+            {
+                notificationText = intent.GetStringExtra("message");
+            }
+
+            // Prepare a notification with vibration, sound and lights
+            var builder = new Notification.Builder(context)
+                  .SetAutoCancel(true)
+                  .SetSmallIcon(Android.Resource.Drawable.IcDialogInfo)
+                  .SetContentTitle(notificationTitle)
+                  .SetContentText(notificationText)
+                  .SetLights(Color.Red, 1000, 1000)
+                  .SetVibrate(new long[] { 0, 400, 250, 400 })
+                  .SetSound(RingtoneManager.GetDefaultUri(RingtoneType.Notification))
+                  .SetContentIntent(PendingIntent.GetActivity(context, 0, new Intent(context, typeof(MainActivity)), PendingIntentFlags.UpdateCurrent));
+
+            // Automatically configure a Notification Channel for devices running Android O+
+            Pushy.SetNotificationChannel(builder, context);
+
+            // Get an instance of the NotificationManager service
+            var notificationManager = (NotificationManager)context.GetSystemService(Context.NotificationService);
+
+            // Build the notification and display it
+            notificationManager.Notify(1, builder.Build());
+        }
+    }
+}
